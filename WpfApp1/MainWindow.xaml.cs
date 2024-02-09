23223e300333f20d9188f9856683e8ce74bd16a4
Դիանա Խաչատրյան <dianakhachatryan11@gmail.com>
2024-02-09 09:31:23 -0800

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Input;
+using System.Windows;
+using System.Windows.Shapes;
+namespace Board2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //  board.MouseLeftButtonDown += Board_MouseLeftButtonDown;
+            board.MouseLeftButtonDown += Board_MouseLeftButtonDown;
+        }
+        private void Board_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Point point = e.GetPosition(board);
+            X.Content = $"X: {point.X}";
+            Y.Content = $"Y: {point.Y}";
+        }
+    }
+}
